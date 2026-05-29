f60c8484452cf03f7660d09d15cc7745a0d30247
Mason Gao <mason2022052@gmail.com>
2026-05-28 21:12:49 -0400

    more

--- assets/js/research-log.js
+++ assets/js/research-log.js
@@ -3,9 +3,9 @@ const researchLogEntries = [
     title: "Summer Research Update",
     date: "2026-05-28",
     body: [
-      "A brief update on what I'm working on this summer: "
-      "1. Still working on improving QSopt_ex: Working on different approaches to MCLU updates. Have some good ideas, but the devil is in the implementation."
-      "2. Working on a new problem that is quite interesting: For a wide body of optimization problems, if you are only able to 'merge' potential solutions, can you still solve the problem in polynomial merges?"
+      "A brief update on what I'm working on this summer.",
+      "1. Still working on improving QSopt_ex with different approaches to MCLU updates. I have some good ideas, but the devil is in the implementation.",
+      "2. Working on a new problem that is quite interesting: for a wide body of optimization problems, if you are only able to merge potential solutions, can you still solve the problem in polynomially many merges?"
     ]
   }
 ];
