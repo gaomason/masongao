42ae9e27b5c876635e5f13d8b13e1c96f3ea31bd
gaomason <mason2022052@gmail.com>
2026-06-05 16:02:49 +0800

    testing dot product implementation

--- assets/js/posts.js
+++ assets/js/posts.js
@@ -3,7 +3,7 @@ const posts = [
     title: "My white hairs",
     date: "2026-06-05",
     body: "I just got a haircut, been overdue, last time I got it cut was the last week of march. The haircut was fine." 
-    + " I was just looking at myself in the mirror - I have a lot of new white hairs. Like at least 20. That's really funny."
+    + " I was just looking at myself in the mirror - I have a lot of new white hairs. Like at least 20. That's really funny.."
     + " I always get white hairs after a stressful period, a stressful semester. But never this many, usually 5-10."
     + " I guess that's just how it's been. Eye bags, eye lines, also more noticable, though I've been sleeping well. Just getting older. Living."
   },
